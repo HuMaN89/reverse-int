7b728988bc1dd63e94be050de4a53fa24512f45b
Kudabaev Bekzhan <Human_B_9@mail.ru>
2024-07-30 11:58:21 +0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = "";
+    n = String(n);
+    if (n[0] === "-") n = n.slice(1);
+    for (let i = n.length - 1; i >= 0; i--) {
+        str += n[i];
+    }
+    return str;
+};
